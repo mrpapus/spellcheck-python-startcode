97e4eb5619eb607814a19e397ac03b6b464a3895
mrpapus <srednalf05@icloud.com>
2023-10-23 14:53:15 -0600

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -2,7 +2,7 @@
 # This start code creates two lists
 # 1: dictionary: a list containing all of the words from "dictionary.txt"
 # 2: aliceWords: a list containing all of the words from "AliceInWonderland.txt"
-3
+import math
 import re  # Needed for splitting text with a regular expression
 
 
@@ -11,10 +11,19 @@ def main():
     dictionary = loadWordsFromFile("data-files/dictionary.txt")
     aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
 
-    # Print first 50 values of each list to verify contents
-    print(dictionary[0:50])
-    print(aliceWords[0:50])
-# end main()
+
+    text = input("dictrionary or aliceWords ")
+    word = input("what word to search for ")
+    method = input("binary or linear search ")
+
+    if method == "binary":
+        print (binarysearch(text,word))
+    else:
+         print (linearSearch(text,word))
+#     # Print first 50 values of each list to verify contents
+#     print(dictionary[0:50])
+#     print(aliceWords[0:50])
+# # end main()
 
 
 def loadWordsFromFile(fileName):
@@ -28,5 +37,39 @@ def loadWordsFromFile(fileName):
 # end loadWordsFromFile()
 
 
+def binarysearch(anArray,item):
+    i = 0
+    lowerindex = 0
+    upperindex = len(anArray)
+    while lowerindex <= upperindex:
+        index = math.floor((lowerindex + upperindex)/2)
+        if anArray[index] < item:
+            lowerindex = index +1
+            i+=1
+        elif  anArray[index] > item:
+            upperindex = index -1
+            i+=1
+        else :
+            print ("program searched",i,"times")
+            return index
+    print ("program searched",i,"times")
+    return -1
+
+
+def linearSearch(anArray,item):
+    i = 0
+    #check array
+    while i < len(anArray) :
+        if anArray[i] == item:
+            print ("program searched",i,"times")
+            return i 
+        # if not at i move to next spot
+        elif anArray[i] != item:
+            i+=1
+            #if the next spot is out of the Array end function and return -1
+            if i == len(anArray):
+                print ("program searched",i,"times")
+                return -1
+        
 # Call main() to begin program
 main()
